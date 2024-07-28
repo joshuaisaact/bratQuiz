ed448e4515c17e5d0b6a73e95fc3e6ff49067ae4
Joshua Tuddenham <joshuaisaact@gmail.com>
2024-07-28 10:51:34 +0100

    Completed question cycle logic, right and wrong answer logic, and start screen logic

--- script.js
+++ script.js
@@ -6,25 +6,45 @@
 
 "use strict";
 
-const questions = {
-  item1: {
+const questions = [
+  {
     name: "bic lighter",
   },
-  item2: {
+  {
     name: "strappy top",
   },
-  item3: {
+  {
     name: "donald trump",
   },
-};
+  {
+    name: "a little key",
+  },
+  {
+    name: "a little line",
+  },
+];
 
 document.querySelector("body").style.backgroundColor = "#ffffff";
 
+const startPage = document.querySelector(".start-page");
+const startButton = document.querySelector(".start-game");
 const guessObj = document.querySelector(".guess-object");
 const guessBrat = document.querySelector(".guess-brat");
 const guessNotBrat = document.querySelector(".guess-notbrat");
 const result = document.querySelector(".result");
+const nextButton = document.querySelector(".next-question");
 const tryAgain = document.querySelector(".try-again");
+let countQ = 0;
+
+const gameStart = function () {
+  document.querySelector("body").style.backgroundColor = "white";
+  startPage.style.display = "none";
+  guessObj.textContent = questions[0].name;
+  guessObj.style.display = "block";
+  startButton.style.display = "none";
+  guessBrat.style.display = "inline-block";
+  guessNotBrat.style.display = "inline-block";
+};
 
 const rightAns = function () {
   document.querySelector("body").style.backgroundColor = "#8acf00";
@@ -33,8 +53,18 @@ const rightAns = function () {
   result.textContent = "correct answer!";
   guessBrat.style.display = "none";
   guessNotBrat.style.display = "none";
-  tryAgain.textContent = "try again?";
-  tryAgain.style.display = "block";
+  //   tryAgain.style.display = "block";
+  nextButton.style.display = "block";
+};
+
+const nextAns = function () {
+  if (countQ < questions.length - 1) {
+    countQ++;
+    return questions[countQ].name;
+  } else {
+    countQ = 0;
+    return questions[0].name;
+  }
 };
 
 const wrongAns = function () {
@@ -44,21 +74,46 @@ const wrongAns = function () {
   result.textContent = "wrong answer";
   guessBrat.style.display = "none";
   guessNotBrat.style.display = "none";
-  tryAgain.textContent = "try again?";
-  tryAgain.style.display = "block";
+  //   tryAgain.style.display = "block";
+  nextButton.style.display = "block";
 };
-// Display result and correct answer
-// Hide guess buttons
-// Display button to try again
-// console.log(guessBrat);
 
-// Logic when game starts
+const nextQuestion = function () {
+  document.querySelector("body").style.backgroundColor = "white";
+  result.style.display = "none";
+  guessObj.textContent = nextAns();
+  guessObj.style.display = "block";
+  guessBrat.style.display = "inline-block";
+  guessNotBrat.style.display = "inline-block";
+  nextButton.style.display = "none";
+};
 
-guessObj.textContent = questions.item1.name;
+const restart = function () {
+  document.querySelector("body").style.backgroundColor = "white";
+  guessObj.textContent = questions.item1.name;
+  tryAgain.style.display = "none";
+  result.style.display = "none";
+  guessObj.style.display = "block";
+  guessBrat.style.display = "inline-block";
+  guessNotBrat.style.display = "inline-block";
+};
 
 guessBrat.addEventListener("click", function () {
   rightAns();
 });
+
 guessNotBrat.addEventListener("click", function () {
   wrongAns();
+});
+
+tryAgain.addEventListener("click", function () {
+  restart();
+});
+
+nextButton.addEventListener("click", function () {
+  nextQuestion();
+});
+
+startButton.addEventListener("click", function () {
+  gameStart();
 });
