347d39c22fa15d09bbfbed1959ca7e4a214da483
Joshua Tuddenham <joshuaisaact@gmail.com>
2024-07-28 07:00:48 +0100

    Initial commit - going through JS theory

---
+++ script.js
@@ -0,0 +1,5 @@
+// Define questions in either an object or an array
+// Needs three main DOM elements - question, answer 1, answer 2
+// Return incorrect or correct
+// Styling when returned?
+// Play again when end of questions
